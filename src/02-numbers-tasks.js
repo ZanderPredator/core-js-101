428b37eeef568fc8e46244c815fb246954a0858f
Brel Nikolay <january_1st@rambler.ru>
2023-09-01 08:10:37 +0300

    fix: string Add

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -20,7 +20,7 @@
  *   5, 5  => 25
  */
 function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+  return arguments[0] * arguments[1]
 }
 
 
@@ -36,7 +36,7 @@ function getRectangleArea(/* width, height */) {
  *   0    => 0
  */
 function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+  return arguments[0] * 2* Math.PI;
 }
 
 /**
@@ -52,7 +52,7 @@ function getCircleCircumference(/* radius */) {
  *  -3, 3  => 0
  */
 function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+  return arguments[0] + arguments[1]
 }
 
 /**
@@ -71,7 +71,7 @@ function getAverage(/* value1, value2 */) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+
 }
 
 /**
@@ -86,8 +86,20 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
+
 function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+  let x = 1;
+  if(a == 1 || a == null){
+    x = -b;
+  };
+
+  while(arguments[0]*x - b != 0){
+    if(arguments[0]*x - b == 0){
+      break;
+    }
+    x++
+  }
+  return x
 }
 
 
@@ -110,7 +122,6 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
 }
 
 /**
@@ -125,8 +136,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit( value ) {
+  return +value.toString().at(-1)
 }
 
 
@@ -142,7 +153,7 @@ function getLastDigit(/* value */) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+  return Number(arguments[0])
 }
 
 /**
@@ -158,8 +169,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal( a, b, c ) {
+  return Math.sqrt(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))
 }
 
 
